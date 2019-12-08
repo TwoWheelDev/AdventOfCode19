210a7b4a0258a72d9b531cb2cb7db3e869a8d103
Daniel Lintott <daniel.j.lintott@gmail.com>
2019-12-08 17:24:49 +0000

    We need to use a list not a tuple

--- day_07/day7.py
+++ day_07/day7.py
@@ -7,12 +7,12 @@ phases = permutations(range(5))
 
 
 for phase in phases:
-    prog_input = [(phase[0], 0)]
+    prog_input = [[phase[0], 0]]
     for i in range(5):
         amp = IntComp("day7_program.txt")
         amp.run_program(prog_input[i])
         if i < 4:
-            prog_input.append((phase[i+1], amp.get_output()))
+            prog_input.append([phase[i+1], amp.get_output()])
         else:
             thruster_output.append(amp.get_output())
 
