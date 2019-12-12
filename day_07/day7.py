647682bf789d021a8fc1100e1095edb4b0a6422a
Daniel Lintott <daniel.j.lintott@gmail.com>
2019-12-12 23:13:06 +0000

    Update day7 pt1 to work with intcode updates

--- day_07/day7.py
+++ day_07/day7.py
@@ -12,8 +12,8 @@ for phase in phases:
         amp = IntComp("day7_program.txt")
         amp.run_program(prog_input[i])
         if i < 4:
-            prog_input.append([phase[i+1], amp.get_output()])
+            prog_input.append([phase[i+1], amp.get_output()[0]])
         else:
-            thruster_output.append(amp.get_output())
+            thruster_output.append(amp.get_output()[0])
 
 print("Max Thruster Output:", max(thruster_output))
